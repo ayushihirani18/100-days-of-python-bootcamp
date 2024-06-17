90f2786ee681e9f94f7eb8c4ff1c7371c9316961
Ayushi Hirani <167391103+ayushihirani18@users.noreply.github.com>
2024-06-17 22:37:28 +0530

    Add files via upload

---
+++ D1-Project_Band_Name_Generator.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the Band Name Generator")
+
+city = input("What is your city name?\n")
+
+pet = input("What is yout pet name?\n")
+
+print("Your band name could be " + city + pet )
+
+print("\n")
